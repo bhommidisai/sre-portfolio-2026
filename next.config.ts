fd603c423397184d355b0162260c4e87d298ac21
sai.bhommidi <sai.bhommidi@gmail.com>
2026-05-24 13:19:53 +0530

    Fix: Moved to root to correctly promote build to docs

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',      // Required for static hosting
+  output: 'export',
+  basePath: '/sre-portfolio-2026', // Add this
+  assetPrefix: '/sre-portfolio-2026', // Add this
   images: {
-    unoptimized: true,   // Required for GitHub Pages
+    unoptimized: true,
   },
 };
 
